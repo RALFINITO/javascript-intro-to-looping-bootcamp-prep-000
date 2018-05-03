3c494af44ace973ae30a78cdfe9773e7df64dce8
rafaelalfinito@hotmail.com <rafaelalfinito@hotmail.com>
2018-05-03 22:27:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(arr){
   arr.push("I am 1 strange loop.")
-  for(var i = 2; i <= 25; i++){
+  for(let i = 2; i <= 25; i++){
     arr.push(`I am ${i} strange loops.`)
   }
   return arr
